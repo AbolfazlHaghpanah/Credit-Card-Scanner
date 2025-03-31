962c1dba6dc59d9c632058813b29687188974c31
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2025-03-31 22:33:57 +0330

    clear project activity

--- app/src/main/java/com/haghpanah/scanner/MainActivity.kt
+++ app/src/main/java/com/haghpanah/scanner/MainActivity.kt
@@ -18,19 +18,18 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
+import androidx.camera.core.impl.Config
 import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.common.util.concurrent.ListenableFuture
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var imageCapture: ImageCapture? = null
-    private var videoCapture: VideoCapture<Recorder>? = null
-    private var recording: Recording? = null
-    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,107 +37,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            requestPermission()
-        }
-
-        // Example of a call to a native method
         binding.viewFinder
-        binding.imageCaptureButton.setOnClickListener {
-            takePhoto()
-        }
-        binding.videoCaptureButton.setOnClickListener {
-            captureVideo()
-        }
     }
 
-    /**
-     * A native method that is implemented by the 'scanner' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-
-    private fun startCamera() {
-        val cameraProviderFeature = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFeature.addListener(
-            {
-                val cameraProvider = cameraProviderFeature.get()
-                val preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-                }
-                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-                try {
-                    // Unbind use cases before rebinding
-                    cameraProvider.unbindAll()
-
-                    // Bind use cases to camera
-                    cameraProvider.bindToLifecycle(
-                        this, cameraSelector, preview
-                    )
-
-                } catch (exc: Exception) {
-                    Log.e(LOG_TAG, "startCamera: $exc")
-                }
-            },
-            ContextCompat.getMainExecutor(this)
-        )
-    }
+    private fun startCamera() {}
 
     private fun requestPermission() {
         activityResultLauncher.launch(REQUIRED_PERMISSIONS)
     }
-
-    private fun takePhoto() {
-        val imageCapture = imageCapture ?: return
-
-        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-            }
-        }
-
-        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(
-                contentResolver,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues
-            )
-            .build()
-
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(LOG_TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val msg = "Photo capture succeeded: ${output.savedUri}"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(LOG_TAG, msg)
-                }
-            }
-        )
-    }
-
-    private fun captureVideo() {}
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it
         ) == PackageManager.PERMISSION_GRANTED
     }
-
-
 
     private val activityResultLauncher =
         registerForActivityResult(
@@ -162,14 +74,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
-    }
-
     companion object {
         private const val LOG_TAG = "cameraError"
-        private const val FILENAME_FORMAT = "image"
         private val REQUIRED_PERMISSIONS =
             mutableListOf<String>(
                 Manifest.permission.CAMERA,
